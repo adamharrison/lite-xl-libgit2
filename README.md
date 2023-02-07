181ab86aa7ded0b35d694330941fdf22ba2c5a68
Adam Harrison <adamdharrison@gmail.com>
2023-02-07 16:45:30 -0500

    Added in manifest.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
+# lite-xl-libgit2
 
+A library which contains libgit2 bindings for lite-xl.
+
+Designed to be used in conjunction with the write-xl project to provide git services on Android.
+
+## Building
+
+### Linux, MSYS, MacOS
+
+```sh
+./build.sh clean && ./build.sh
+```
+
+### Cross Compile Linux -> Windows
+
+```sh
+./build.sh clean && BIN=libgit2.dll CC=x86_64-w64-mingw32-gcc AR=x86_64-w64-mingw32-gcc-ar WINDRES=x86_64-w64-mingw32-windres \
+  CMAKE_DEFAULT_FLAGS="-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER\ -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=NEVER -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SYSTEM_INCLUDE_PATH=/usr/share/mingw-w64/include"\
+  GIT2_CONFIGURE="-DDLLTOOL=x86_64-w64-mingw32-dlltool" ./build.sh
+```
+
+### Cross Compile Linux -> Android
+
+```sh
+./build.sh clean &&
+```
--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 : ${CC=gcc}
 : ${AR=ar}
 : ${MAKE=make}
+: ${CMAKE=cmake}
 : ${BIN=libgit2.so}
 : ${JOBS=4}
 
@@ -10,10 +11,10 @@ SRCS="src/*.c"
 CFLAGS="$CFLAGS -Ilib/prefix/include -fPIC"
 LDFLAGS="$LDFLAGS -lm -Llib/prefix/lib"
 
-[[ "$@" == "clean" ]] && rm -rf lib/libgit2/build lib/zlib/build lib/libzip/build lib/mbedtls-2.27.0/build lib/prefix lua $BIN *.exe src/lpm.luac src/lpm.lua.c && exit 0
-cmake --version >/dev/null 2>/dev/null || { echo "Please ensure that you have cmake installed." && exit -1; }
+[[ "$@" == "clean" ]] && rm -rf lib/libgit2/build lib/zlib/build lib/mbedtls-2.27.0/build lib/prefix $BIN *.exe src/lpm.luac src/lpm.lua.c && exit 0
+$CMAKE --version >/dev/null 2>/dev/null || { echo "Please ensure that you have cmake installed." && exit -1; }
 
-# Build supporting libraries, libz, libmbedtls, libmbedcrypto, libgit2, libzip, libmicrotar, liblua
+# Build supporting libraries, libz, libmbedtls, libmbedcrypto, libgit2
 CMAKE_DEFAULT_FLAGS=" $CMAKE_DEFAULT_FLAGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=`pwd`/lib/prefix -DCMAKE_INSTALL_PREFIX=`pwd`/lib/prefix -DBUILD_SHARED_LIBS=OFF"
 mkdir -p lib/prefix/include lib/prefix/lib
 if [[ "$@" != *"-lz"* ]]; then
@@ -22,15 +23,15 @@ if [[ "$@" != *"-lz"* ]]; then
   LDFLAGS="$LDFLAGS -lz"
 fi
 if [[ "$@" != *"-lmbedtls"* && "$@" != *"-lmbedcrypto"* ]]; then
-  [ ! -e "lib/mbedtls-2.27.0/build" ] && cd lib/mbedtls-2.27.0 && mkdir build && cd build && CFLAGS="$CFLAGS $CFLAGS_MBEDTLS -DMBEDTLS_MD4_C=1 -w" cmake .. $CMAKE_DEFAULT_FLAGS  -G "Unix Makefiles" -DENABLE_TESTING=OFF -DENABLE_PROGRAMS=OFF $SSL_CONFIGURE && CFLAGS="$CFLAGS $CFLAGS_MBEDTLS -DMBEDTLS_MD4_C=1 -w" $MAKE -j $JOBS && $MAKE install && cd ../../../
+  [ ! -e "lib/mbedtls-2.27.0/build" ] && cd lib/mbedtls-2.27.0 && mkdir build && cd build && CFLAGS="$CFLAGS $CFLAGS_MBEDTLS -DMBEDTLS_MD4_C=1 -w" $CMAKE .. $CMAKE_DEFAULT_FLAGS  -G "Unix Makefiles" -DENABLE_TESTING=OFF -DENABLE_PROGRAMS=OFF $SSL_CONFIGURE && CFLAGS="$CFLAGS $CFLAGS_MBEDTLS -DMBEDTLS_MD4_C=1 -w" $MAKE -j $JOBS && $MAKE install && cd ../../../
   LDFLAGS="$LDFLAGS -lmbedtls -lmbedx509 -lmbedcrypto"
 fi
 if [[ "$@" != *"-lgit2"* ]]; then
-  [ ! -e "lib/libgit2/build" ] && cd lib/libgit2 && mkdir build && cd build && CFLAGS="$CFLAGS $CFLAGS_LIBGIT2" cmake .. -G "Unix Makefiles" $GIT2_CONFIGURE $CMAKE_DEFAULT_FLAGS -DBUILD_TESTS=OFF -DBUILD_CLI=OFF -DREGEX_BACKEND=builtin -DUSE_SSH=OFF -DUSE_HTTPS=mbedTLS && CFLAGS="$CFLAGS $CFLAGS_LIBGIT2" $MAKE -j $JOBS && $MAKE install && cd ../../../
+  [ ! -e "lib/libgit2/build" ] && cd lib/libgit2 && mkdir build && cd build && CFLAGS="$CFLAGS $CFLAGS_LIBGIT2" $CMAKE .. -G "Unix Makefiles" $GIT2_CONFIGURE $CMAKE_DEFAULT_FLAGS -DBUILD_TESTS=OFF -DBUILD_CLI=OFF -DREGEX_BACKEND=builtin -DUSE_SSH=OFF -DUSE_HTTPS=mbedTLS && CFLAGS="$CFLAGS $CFLAGS_LIBGIT2" $MAKE -j $JOBS && $MAKE install && cd ../../../
   LDFLAGS="-lgit2 $LDFLAGS"
 fi
 
-[[ $OSTYPE != 'msys'* && $CC != *'mingw'* && $CC != "emcc" ]] && CFLAGS="$CFLAGS -DLUA_USE_LINUX" && LDFLAGS="$LDFLAGS -ldl"
+[[ $OSTYPE != 'msys'* && $CC != *'mingw'* && $CC != "emcc" ]] && LDFLAGS="$LDFLAGS -ldl"
 [[ $OSTYPE == 'msys'* || $CC == *'mingw'* ]]                  && LDFLAGS="$LDFLAGS -lbcrypt -lws2_32 -lz -lwinhttp -lole32 -lcrypt32 -lrpcrt4"
 [[ $OSTYPE == *'darwin'* ]]                                   && LDFLAGS="$LDFLAGS -liconv -framework Security -framework Foundation"
 
